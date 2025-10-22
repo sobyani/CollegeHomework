ee9c3de956136b75c27212b3050e87dc29110f47
sobyani <sobaninaroslav794@gmail.com>
2025-10-22 22:03:15 +0300

    Add files via upload

---
+++ дз по компьютерной школе 8/Program.cs
@@ -0,0 +1,26 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+
+        Console.WriteLine("Введите новое число,чтобы вычеслить факториал:");
+        string Number = Console.ReadLine();
+        int n = Convert.ToInt32(Number);
+        int a = 1;
+        if (n < 0)
+        {
+            Console.WriteLine("это невозможно");
+            return;
+        }
+        for (int i = 1; i <= n;  i++)
+        {
+            a  = a  * i;
+            if (i == n)
+            {
+
+                Console.WriteLine($"факториал числа {n}:{a}");
+            }
+            
+        }
+    }
+}
