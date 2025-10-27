6040662ff640f740546442ee6f6c7b24cc781f2e
sobyani <sobaninaroslav794@gmail.com>
2025-10-27 23:11:05 +0300

    Add files via upload

---
+++ дз по компьютерной школе 9/дз по компьютерной школе 9/Program.cs
@@ -0,0 +1,24 @@
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+        int[] originalArray = { 10, 20, 30, 40, 50 };
+        Console.Write("Исходный массив: ");
+        for (int i = 0; i < originalArray.Length; i++)
+        {
+            Console.Write(originalArray[i] + " ");
+        }
+        Console.WriteLine();
+        int[] reversedArray = new int[originalArray.Length];
+        for (int i = 0; i < originalArray.Length; i++)
+        {
+            reversedArray[i] = originalArray[originalArray.Length - 1 - i];
+        }
+        Console.Write("Перевернутый массив: ");
+        for (int i = 0; i < reversedArray.Length; i++)
+        {
+            Console.Write(reversedArray[i] + " ");
+        }
+        Console.WriteLine(); 
+    }
+}
