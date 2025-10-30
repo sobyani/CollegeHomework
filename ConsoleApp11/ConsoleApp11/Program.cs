e529962b4692c480cace193ff39f0a351f491c5a
sobyani <sobaninaroslav794@gmail.com>
2025-10-30 20:18:22 +0300

    Add files via upload

---
+++ ConsoleApp11/ConsoleApp11/Program.cs
@@ -0,0 +1,22 @@
+﻿using ConsoleApp11;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("--- Создаем  прямоугольник 1 ---");
+        Rectangle rect1 = new Rectangle(10, 5);
+
+        Console.WriteLine($"Ширина: {rect1.Width}");
+        Console.WriteLine($"Высота: {rect1.Height}");
+        Console.WriteLine($"Площадь: {rect1.GetArea()}");
+        Console.WriteLine($"Периметр: {rect1.GetPerimeter()}");
+
+        Console.WriteLine("--- Создаем прямоугольник 1 ---");
+        Rectangle rect2 = new Rectangle(15, 8);
+        Console.WriteLine($"Ширина rect2: {rect2.Width}"); 
+        Console.WriteLine($"Высота rect2: {rect2.Height}");
+        Console.WriteLine($"Площадь: {rect2.GetArea()}");
+        Console.WriteLine($"Периметр: {rect2.GetPerimeter()}");
+    }
+}
---
+++ ConsoleApp11/ConsoleApp11/Rectangle.cs
@@ -0,0 +1,62 @@
+﻿namespace ConsoleApp11
+{
+    internal class Rectangle
+    {
+        private double _width;
+        private double _height;
+
+        public double Width
+        {
+            get
+            {
+                return _width;
+            }
+            set
+            {
+                if (value > 0)
+                {
+                    _width = value;
+                }
+                else
+                {
+                    Console.WriteLine("Ошибка: ширина должна быть положительным числом!");
+                }
+            }
+        }
+
+        public double Height
+        {
+            get
+            {
+                return _height;
+            }
+            set
+            {
+                if (value > 0)
+                {
+                    _height = value;
+                }
+                else
+                {
+                    Console.WriteLine("Ошибка: высота должна быть положительным числом!");
+                }
+            }
+        }
+
+        public Rectangle(double width, double height)
+        {
+            Width = width;
+            Height = height;
+        }
+
+        public double GetArea()
+        {
+            return _width * _height;
+        }
+
+        public double GetPerimeter()
+        {
+            return 2 * (_width + _height);
+        }
+    }
+}
