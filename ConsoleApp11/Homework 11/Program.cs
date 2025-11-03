aee9a953095df7e09c020039827e12eb89d7a95c
sobyani <sobaninaroslav794@gmail.com>
2025-11-03 18:35:50 +0300

    Add files via upload

---
+++ ConsoleApp11/Homework 11/Program.cs
@@ -0,0 +1,25 @@
+﻿using Homework_11;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("--- Создание профилей студентов ---");
+
+        var student1 = new Student("Иван", "Петров", 19);
+        Console.WriteLine("Студент 1:");
+        student1.Introduce();
+        Console.WriteLine($"Полное имя: {student1.FullName}");
+
+        var student2 = new Student("Анна", "Сидорова", 20);
+        Console.WriteLine("Студент 2:");
+        student2.Introduce();
+        Console.WriteLine($"Полное имя: {student2.FullName}");
+
+        Console.WriteLine("--- Попытка изменить возраст на некорректное значение ---");
+
+        student1.Age = 150;
+
+        Console.WriteLine($"Текущий возраст студента 1: {student1.Age}");
+    }
+}
---
+++ ConsoleApp11/Homework 11/Student.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_11
+{
+    internal class Student
+    {
+        private int _age;
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public int Age
+        {
+            get => _age;
+            set
+            {
+                if (value >= 6 && value <= 100)
+                {
+                    _age = value;
+                }
+                else
+                {
+                    Console.WriteLine("Ошибка: возраст должен быть в диапазоне от 6 до 100!");
+                }
+            }
+        }
+
+        public string FullName => $"{FirstName} {LastName}".Trim();
+
+        public Student(string firstName, string lastName, int age)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+        }
+
+        public void Introduce()
+        {
+            Console.WriteLine($"Привет, меня зовут {FullName}, мне {Age} лет.");
+        }
+    }
+}
