abcf757b403d959d3924612d3ab77457077476d1
sobyani <sobaninaroslav794@gmail.com>
2025-12-31 19:37:41 +0300

    Add files via upload

---
+++ Homework5/CacheService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework5
+{
+    public sealed class CacheService
+    {
+        private static readonly CacheService _instance = new CacheService();
+
+        private readonly Dictionary<string, object> _cache = new Dictionary<string, object>();
+
+        private CacheService()
+        {
+            Console.WriteLine("(Экземпляр CacheService создан)");
+        }
+
+        public static CacheService GetInstance()
+        {
+            return _instance;
+        }
+        public void Add(string key, object value)
+        {
+            _cache[key] = value;
+            Console.WriteLine($"Данные '{key}' добавлены.");
+        }
+
+        public object? Get(string key)
+        {
+            return _cache.ContainsKey(key) ? _cache[key] : null;
+        }
+    }
+}
---
+++ Homework5/Program.cs
@@ -0,0 +1,28 @@
+﻿using Homework5;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("--- Демонстрация работы глобального кэша (Singleton) ---");
+
+        CacheService cache1 = CacheService.GetInstance();
+
+        CacheService cache2 = CacheService.GetInstance();
+
+        Console.WriteLine("Добавляем данные в кэш через первую ссылку...");
+        cache1.Add("ConnectionString", "Server=.;Database=CacheDB;");
+        cache1.Add("ApiKey", "XYZ12345ABC");
+
+        Console.WriteLine("Получаем данные из кэша через ВТОРУЮ ссылку...");
+        var connectionString = cache2.Get("ConnectionString");
+        var apiKey = cache2.Get("ApiKey");
+
+        Console.WriteLine($"Значение по ключу 'ConnectionString': {connectionString}");
+        Console.WriteLine($"Значение по ключу 'ApiKey': {apiKey}");
+
+        Console.WriteLine("Проверяем, что обе переменные ссылаются на один объект...");
+        bool areSame = object.ReferenceEquals(cache1, cache2);
+        Console.WriteLine($"Результат: {areSame}");
+    }
+}
