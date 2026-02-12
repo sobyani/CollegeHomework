58804fc05aba9348953c477c9e617a139f78d5dc
sobyani <sobaninaroslav794@gmail.com>
2026-02-12 18:30:10 +0300

    Add files via upload

---
+++ Homework 7/NotificationService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_7
+{
+    public class NotificationService : INotificationService
+    {
+        public void SendNotification()
+        {
+            Console.WriteLine("Отправка email-уведомления: 'Ваш заказ принят'.");
+        }
+    }
+}
